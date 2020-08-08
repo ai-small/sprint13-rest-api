7dcc8601295e79dad33adce4c17cecea484e4752
ai-small <aismall@yandex.ru>
2020-08-09 00:22:08 +1200

    open connection to db and create schemas without validation of links

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -6,6 +7,12 @@ const notFound = require('./routes/notFound');
 
 const app = express();
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
