f56c359a6ea29ecbe325d7f0282c4b10f7f8943f
ai-small <aismall@yandex.ru>
2020-08-10 01:40:37 +1200

    create controllers and routes for cards

---
+++ controllers/cards.js
@@ -0,0 +1,28 @@
+const Card = require('../models/card');
+
+const getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const createCard = (req, res) => {
+  const { name, link } = req.body;
+  const owner = req.user._id;
+
+  Card.create({ name, link, owner })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCard,
+};
--- routes/cards.js
+++ routes/cards.js
@@ -1,19 +1,8 @@
 const cardsRouter = require('express').Router();
-const path = require('path');
-const fs = require('fs');
+const { getCards, createCard, deleteCard } = require('../controllers/cards');
 
-cardsRouter.get('/', (req, res) => {
-  const cardsPath = path.join(__dirname, '../data', 'cards.json');
-  const reader = fs.createReadStream(cardsPath, { encoding: 'utf-8' });
-
-  reader.on('open', () => {
-    res.writeHead(200, { 'Content-type': 'application/json; charset=utf-8' });
-    reader.pipe(res);
-  });
-
-  reader.on('error', (err) => {
-    res.status('500').send({ Error: err.message });
-  });
-});
+cardsRouter.get('/', getCards);
+cardsRouter.post('/', createCard);
+cardsRouter.delete('/:cardId', deleteCard);
 
 module.exports = cardsRouter;
