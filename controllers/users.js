1277e9ba37a6ed7a0b028a04c0a1c3061337efa4
ai-small <aismall@yandex.ru>
2020-08-12 16:01:36 +1200

    updating user's profile and avatar

--- controllers/users.js
+++ controllers/users.js
@@ -27,8 +27,48 @@ const createUser = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true,
+    },
+  )
+    .then((user) => {
+      if (user) {
+        res.status(200).send({ data: user });
+      } else res.status(404).send({ message: 'User not found' });
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true,
+    },
+  )
+    .then((user) => {
+      if (user) {
+        res.status(200).send({ data: user });
+      } else res.status(404).send({ message: 'User not found' });
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
 module.exports = {
   getUsers,
   getUserById,
   createUser,
+  updateUser,
+  updateAvatar,
 };
